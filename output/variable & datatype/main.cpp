6d1a14ef76c7a4002f1c2cd554c236de5c1ec364
soumya569 <soumyaranjanroul38@gmail.com>
2025-08-23 21:11:45 +0530

    array code added

--- output/variable & datatype/main.cpp
+++ output/variable & datatype/main.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
 
 using namespace std;
-int main(){
 
+void print(int arr[], int size){
+    for(int index =0;index<size;index++){
+        cout<< arr[index]<<" ";
+    }
+}
+void solve1(int arr[],int n){
+    for (int i=0;i<n;i++ )
+    arr[i]=arr[i]*10;
+}
+int main(){
+    int arr[]={1,2,3,4,5};
+    int size=5;
+    solve1(arr,size);
+    print(arr,size);
+    
     //variable define
     //int age = 20;
     //cout << "my age is: " << age <<endl;
@@ -22,13 +36,21 @@ int main(){
 
     // return 0;
 
-    int arr[10];
-    for (int index=0;index<5;index++){
-        cout<<"Enter the value for box index: "<<index<<endl;
-        cin>>arr[index];
-    }
-    for (int index=0;index<5;index++){
-        cout<<arr[index]<<" ";
-    }
+    // int arr[10];
+    // for (int index=0;index<5;index++){
+    //     cout<<"Enter the value for box index: "<<index<<endl;
+    //     cin>>arr[index];
+    // }
+    // for (int index=0;index<5;index++){
+    //     cout<<arr[index]<<" ";
+    // }
+    // return 0;
+    // for (int index=0;index<5;index++){
+    //     cout<<"Enter the value for box index: "<<index<<endl;
+    //     cin>>arr[index];
+    // }
+    // for (int index=0;index<5;index++){
+    //     cout<<index[arr]<<" ";
+    // }
     return 0;
 }
